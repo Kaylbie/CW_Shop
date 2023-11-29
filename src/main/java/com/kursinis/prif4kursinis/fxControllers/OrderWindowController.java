606d51c1bf8b7bf1a173a63c175af79fbfa94180
kaylbie <audrius0917@gmail.com>
2023-11-29 19:03:43 +0200

    Simple dashboard gui (orders)

---
+++ src/main/java/com/kursinis/prif4kursinis/fxControllers/OrderWindowController.java
@@ -0,0 +1,50 @@
+package com.kursinis.prif4kursinis.fxControllers;
+
+import com.kursinis.prif4kursinis.StartGui;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OrderWindowController implements Initializable {
+
+
+
+    @FXML
+    public VBox ordersVbox;
+    @FXML
+    public ScrollPane ordersScrollPane;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        refreshNodes();
+    }
+    private void refreshNodes() {
+        ordersVbox.getChildren().clear();
+        Node[] nodes = new Node[10];
+        for(int i = 0; i < nodes.length; i++) {
+            try {
+                nodes[i] = FXMLLoader.load(StartGui.class.getResource("/com/kursinis/prif4kursinis/order-tab-window.fxml"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            ordersVbox.getChildren().add(nodes[i]);
+        }
+
+    }
+
+    public void updateOrderTabs(MouseEvent mouseEvent) {
+        refreshNodes();
+    }
+}
---
+++ src/main/java/com/kursinis/prif4kursinis/hibernateControllers/CustomHib.java
@@ -0,0 +1,60 @@
+package com.kursinis.prif4kursinis.hibernateControllers;
+
+import com.kursinis.prif4kursinis.model.Product;
+import com.kursinis.prif4kursinis.model.User;
+import com.kursinis.prif4kursinis.model.Warehouse;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+public class CustomHib extends GenericHib {
+    public CustomHib(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
+    }
+
+    public User getUserByCredentials(String login, String password) {
+        EntityManager em = null;
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<User> query = cb.createQuery(User.class);
+            Root<User> root = query.from(User.class);
+            query.select(root).where(cb.and(cb.like(root.get("login"), login), cb.like(root.get("password"), password)));
+            Query q;
+
+            q = em.createQuery(query);
+            return (User) q.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            if (em != null) em.close();
+        }
+    }
+
+    public void deleteProduct(int id) {
+
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            var product = em.find(Product.class, id);
+            //Kai turiu objekta, galiu ji "nulinkint"
+
+            Warehouse warehouse = product.getWarehouse();
+            if (warehouse != null) {
+                warehouse.getInStockProducts().remove(product);
+                em.merge(warehouse);
+            }
+
+            em.remove(product);
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
